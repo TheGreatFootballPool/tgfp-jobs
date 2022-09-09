0f30361962b23c9567a95093b94d6e24df62f8ab
John Sturgeon <john.sturgeon@me.com>
2022-09-09 07:11:32 -0400

    Updated to latest libraries

---
+++ bump_version_and_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# usage: [major, minor, patch] "commit message"
+poetry version $1
+git add .
+git commit -m "$2"
+git push
+NEW_VERSION=`poetry version -s`
+git tag v${NEW_VERSION}
+git push origin v${NEW_VERSION}
+# TODO: Add code to push to the production server via ssh
