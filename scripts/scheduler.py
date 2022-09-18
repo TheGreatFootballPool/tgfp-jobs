49d87440db4ba9359bf7620bd4619661270ccd19
John Sturgeon <john.sturgeon@me.com>
2022-09-17 19:59:55 -0700

    Fixed timing to fit the cron job a bit better

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -8,9 +8,8 @@ from send_message_to_admin import send_message
 
 WIN_LOSS_JOB_TAG = 'win-loss-job'
 
-MON_THU_START_TIME = "16:00"
-SAT_SUN_START_TIME = "09:00"
-ALL_GAMES_END_TIME = "22:00"
+START_TIME = "09:00"
+END_TIME = "22:00"
 
 logger = logging.getLogger('mylogger')
 
@@ -40,20 +39,20 @@ def load_nfl_schedule():
     """ This is the schedule of when NFL games run """
 
     # Monitor Thursday night games
-    schedule.every().thursday.at(MON_THU_START_TIME).do(start_updating_win_loss)
-    schedule.every().thursday.at(ALL_GAMES_END_TIME).do(stop_updating_win_loss)
+    schedule.every().thursday.at(START_TIME).do(start_updating_win_loss)
+    schedule.every().thursday.at(END_TIME).do(stop_updating_win_loss)
 
     # Monitor Saturday games (when they start)
-    schedule.every().saturday.at(SAT_SUN_START_TIME).do(start_updating_win_loss)
-    schedule.every().saturday.at(ALL_GAMES_END_TIME).do(stop_updating_win_loss)
+    schedule.every().saturday.at(START_TIME).do(start_updating_win_loss)
+    schedule.every().saturday.at(END_TIME).do(stop_updating_win_loss)
 
     # Monitor Sunday all games throughout the day
-    schedule.every().sunday.at(SAT_SUN_START_TIME).do(start_updating_win_loss)
-    schedule.every().sunday.at(ALL_GAMES_END_TIME).do(stop_updating_win_loss)
+    schedule.every().sunday.at(START_TIME).do(start_updating_win_loss)
+    schedule.every().sunday.at(END_TIME).do(stop_updating_win_loss)
 
     # Monitor Monday Night Football
-    schedule.every().monday.at(MON_THU_START_TIME).do(start_updating_win_loss)
-    schedule.every().monday.at(ALL_GAMES_END_TIME).do(stop_updating_win_loss)
+    schedule.every().monday.at(START_TIME).do(start_updating_win_loss)
+    schedule.every().monday.at(END_TIME).do(stop_updating_win_loss)
 
 
 def main():
@@ -67,7 +66,7 @@ def main():
     day = dt.weekday()
     hour = dt.hour
     if day in [0, 3, 4, 5]:
-        if hour < 22:
+        if 8 < hour < 22:
             start_updating_win_loss()
     send_message("The Scheduler is up and running!")
     while True:
