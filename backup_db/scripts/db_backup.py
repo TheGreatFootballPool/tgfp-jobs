c5c94a7fb9ada531a06bf8676570c30de7632f29
John Sturgeon <john.sturgeon@me.com>
2022-09-30 14:04:23 -0700

    Fixed file naming bug

--- backup_db/scripts/db_backup.py
+++ backup_db/scripts/db_backup.py
@@ -11,15 +11,8 @@ MONGO_BACKUP_URI = os.getenv('MONGO_BACKUP_URI')
 BACKUP_DIR = os.getenv('BACKUP_DIR')
 
 logging.basicConfig(level=logging.INFO)
-dt = datetime.now()
-month = dt.month
 
 FILENAME = f"{BACKUP_DIR}/tgfp"
-HOURLY_FILE = f"{FILENAME}.hourly.{dt.hour}.gz"
-DAILY_FILE = f"{FILENAME}.daily.{dt.day}.gz"
-MONTHLY_FILE = f"{FILENAME}.monthly.{dt.month}.gz"
-YEARLY_FILE = f"{FILENAME}.yearly.{dt.year}.gz"
-
 CMD = [
     "mongodump",
     f"--uri={MONGO_BACKUP_URI}",
@@ -29,6 +22,12 @@ CMD = [
 
 
 def db_backup():
+    dt = datetime.now()
+    hourly_file = f"{FILENAME}.hourly.{dt.hour}.gz"
+    daily_file = f"{FILENAME}.daily.{dt.day}.gz"
+    monthly_file = f"{FILENAME}.monthly.{dt.month}.gz"
+    yearly_file = f"{FILENAME}.yearly.{dt.year}.gz"
+
     logging.info("About to dump mongo DB using command")
     try:
         output = check_output(CMD, stderr=subprocess.STDOUT).decode("utf-8")
@@ -37,10 +36,10 @@ def db_backup():
 
     if 'done dumping' not in output:
         raise RuntimeError('Mongo DB not backed up')
-    shutil.copyfile(FILENAME, HOURLY_FILE)
-    shutil.copyfile(FILENAME, DAILY_FILE)
-    shutil.copyfile(FILENAME, MONTHLY_FILE)
-    shutil.copyfile(FILENAME, YEARLY_FILE)
+    shutil.copyfile(FILENAME, hourly_file)
+    shutil.copyfile(FILENAME, daily_file)
+    shutil.copyfile(FILENAME, monthly_file)
+    shutil.copyfile(FILENAME, yearly_file)
     os.remove(FILENAME)
     urllib.request.urlopen(
         "https://hc-ping.com/4b181d15-e36d-44f4-b7d4-498ab03f71c7",
