2af3eee15f8d03bcf74418adb59851cdd3b46a3a
John Sturgeon <john.sturgeon@me.com>
2023-09-15 18:42:52 -0700

    Fix secret loading bug

--- tgfp_jobs/scripts/prefect_helpers/helpers.py
+++ tgfp_jobs/scripts/prefect_helpers/helpers.py
@@ -8,7 +8,7 @@ from prefect.blocks.system import Secret
 def get_secret(secret_name: str, is_var: bool = False, use_env: bool = True) -> str:
     """ Retrieves the secret or variable, using the current environment """
     env: str = os.getenv('ENVIRONMENT')
-    env_string: str = env if use_env else ""
+    env_string: str = f"-{env}" if use_env else ""
     if is_var:
         return str(variables.get(f"{secret_name}_{env_string}"))
-    return Secret.load(f"{secret_name}-{env_string}").get()
+    return Secret.load(f"{secret_name}{env_string}").get()
