c6e32102186b7353b71cdb161af3804c82e8a2b0
John Sturgeon <john.sturgeon@me.com>
2023-09-20 20:34:23 -0700

    created a new entrypoint

--- tgfp_jobs/scripts/entrypoint.sh
+++ tgfp_jobs/scripts/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "CDing to scripts"
 cd /app/scripts
+echo "Deploying Prefect"
 prefect --no-prompt deploy --all
+echo "Starting Worker Pool"
 prefect worker start --pool 'tgfp-jobs'
