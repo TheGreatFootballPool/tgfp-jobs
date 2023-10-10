468e3b0c58fce40d80c772e67cce7cb533baee9b
John Sturgeon <john.sturgeon@me.com>
2023-10-10 10:54:46 -0700

    Updated flow deployment

--- tgfp_jobs/scripts/bump_version_and_deploy.sh
+++ tgfp_jobs/scripts/bump_version_and_deploy.sh
@@ -6,7 +6,7 @@ git commit -m "$2"
 git push
 export NEW_VERSION=`poetry version -s`
 git tag v${NEW_VERSION}
-git push origin v${NEW_VERSION}
+git push origin ${NEW_VERSION}
 echo "Pushing to server"
 ssh goshdarnedserver.lan 'cd ~/tgfp ; git pull --rebase'
-./tgfp_jobs/scripts/deploy_production.sh
+./deploy_production.sh
