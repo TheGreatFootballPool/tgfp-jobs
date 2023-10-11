d9c090e76df6b746da2145540edf70606d04eced
John Sturgeon <john.sturgeon@me.com>
2023-10-11 13:55:36 -0700

    Updated Entrypoint Script

--- tgfp_jobs/scripts/entrypoint.sh
+++ tgfp_jobs/scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "CDing to scripts"
 cd /app/scripts
-echo "Deploying Prefect"
-prefect --no-prompt deploy --all
 echo "Starting Worker Pool"
 prefect worker start --pool 'tgfp-jobs'
