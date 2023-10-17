5a7bc8763a55fdff01d3f947083ec371f8247b92
John Sturgeon <john.sturgeon@me.com>
2023-10-17 07:37:18 -0700

    Fixed issue #30

--- tgfp_jobs/scripts/update_game_score.py
+++ tgfp_jobs/scripts/update_game_score.py
@@ -32,8 +32,10 @@ def run_update_game(tgfp_nfl_game_id: str):
     # Wait 1 minute, then update the team records before exiting
     logger.info("Game is done, waiting 1 minute, updating scores, then exiting...")
     sleep(60)
-    update_game(tgfp_nfl_game_id)
-    logger.info("Got a final for the game, exiting run flow")
+    tgfp = TGFP(config.MONGO_URI)
+    nfl_data_source = TgfpNfl(week_no=tgfp.current_week())
+    logger.info("Got a final for the game, updating team records, then exiting run flow")
+    _update_team_records(TGFP(tgfp), nfl_data_source)
 
 
 def update_game(tgfp_nfl_game_id: str) -> bool:
@@ -64,6 +66,7 @@ def _update_scores(nfl_data_source, tgfp_game: TGFPGame) -> bool:
     @param tgfp_game: game to update
     @return: True if game is final, otherwise, false
     """
+    logger = get_run_logger()
     nfl_game = nfl_data_source.find_game(game_id=tgfp_game.tgfp_nfl_game_id)
     if not nfl_game.is_pregame:
         if tgfp_game.home_team_score != int(nfl_game.total_home_points) or \
@@ -74,7 +77,6 @@ def _update_scores(nfl_data_source, tgfp_game: TGFPGame) -> bool:
             tgfp_game.game_status = nfl_game.game_status_type
 
     tgfp_game.save()
-    logger = get_run_logger()
     log_msg = f"Game state: {tgfp_game.game_status}"
     logger.info(log_msg)
     return tgfp_game.is_final
